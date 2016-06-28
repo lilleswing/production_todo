a408003f6ead5de0cbaf1e4f981e34750d76079b
Karl Leswing <lilleswing@gmail.com>
2016-06-28 16:48:34 -0400

    Configure for webpack but inclined to use browserify for django compatability for dev

---
+++ todo-js/webpack.config.js
@@ -0,0 +1,37 @@
+var debug = process.env.NODE_ENV !== "production";
+var webpack = require('webpack');
+
+module.exports = {
+    context: __dirname,
+    devtool: debug ? "inline-sourcemap" : null,
+    entry: "./js/app.js",
+    output: {
+        path: __dirname + "/js",
+        filename: "app.min.js"
+    },
+    plugins: debug ? [] : [
+        new webpack.optimize.DedupePlugin(),
+        new webpack.optimize.OccurenceOrderPlugin(),
+        new webpack.optimize.UglifyJsPlugin({mangle: false, sourcemap: false})
+    ],
+    resolve: {
+        extensions: ['', '.js', '.jsx']
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.jsx?$/,
+                exclude: /node_modules/,
+                loaders: ['react-hot', 'babel-loader?presets[]=react,presets[]=es2015'],
+                //loaders: ["react-hot", 'babel-loader'],
+                //query: {
+                //    presets : ['es2015', 'react']
+                //}
+            },
+            {
+                test: /\.html$/,
+                loader: "file?name=[name].[ext]"
+            }
+        ]
+    }
+};
